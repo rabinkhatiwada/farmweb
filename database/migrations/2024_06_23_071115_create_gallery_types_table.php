9b383b7346348e98e2237c2d2feb410ca45d6fbf
Monu Kumar <monu.deltatech@gmail.com>
2024-07-09 12:41:09 +0545

    migration manage for gallery_type

--- database/migrations/2024_06_23_071115_create_gallery_types_table.php
+++ database/migrations/2024_06_23_071115_create_gallery_types_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('gallery_types', function (Blueprint $table) {
             $table->id();
+            $table->text('slug');
+
+ 
             $table->string('title'); 
 
             $table->timestamps();
